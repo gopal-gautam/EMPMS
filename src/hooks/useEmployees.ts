f25f9530579bfde03a16ff4a19475fdc28ec5142
gopalg <gopal@gopal>
2025-12-30 22:28:08 +1100

    add clock-in-out api

--- src/hooks/useEmployees.ts
+++ src/hooks/useEmployees.ts
@@ -3,6 +3,6 @@ import { getEmployees } from '../api/employees';
 
 export const useEmployees = () => useQuery({
     queryKey: ['employees'],
-    queryFn: getEmployees,
+    queryFn: () => getEmployees(),
     staleTime: 5 * 60 * 1000,
 });
